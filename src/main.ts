fa4cf64082ff2aa1a98d351755c8b5fef723a1d9
ar41bald <ar41bald.ryzhkov@gmail.com>
2019-11-12 04:52:02 -0500

    fix(main): set port for heroku correctly

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
   }));
   app.useGlobalFilters(new QueryFailedErrorFilter());
   app.useGlobalInterceptors(new TransformerInterceptor());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
